a8243e128d377b2e59957409cdb5df3c5619fc0e
arturomf94 <arturomf94@gmail.com>
2019-07-04 19:22:31 -0500

    make slimes get away from boundaries

--- slime-moulds/ofApp.cpp
+++ slime-moulds/ofApp.cpp
@@ -12,30 +12,33 @@ void ofApp::makePoint(int posx, int posy){
 //--------------------------------------------------------------
 void ofApp::setup(){
 
-  // Create N "points" randomly distributed in grid (1000 x 1000)
+  // Create N points in 1000 x 1000 grid.
 
   mesh.setMode(OF_PRIMITIVE_POINTS);
-  int N = 10000;
-  int start_point_x = 400 + rand() % 100;
-  int start_point_y = 400 + rand() % 100;
+  int N = 100000;
+  int start_point_x = 450 + rand() % 100;
+  int start_point_y = 450 + rand() % 100;
   int x = start_point_x;
   int y = start_point_y;
   int i = 0;
+  int w = 1000;
+  int h = 1000;
   while (i < N){
-    if (x >= 1000) {
-      x = 999;
+    if (x >= w ){
+      // Slime could get "trapped" near boundaries, so look somewhere else.
+      x = rand() % 1000;
     } else {
-      if (x < 0 ) {
-        x = 0;
+      if (x < 0 ){
+        x = rand() % 1000;
       } else {
         x = x + rand() % 3 * (int)(pow(-1, i));
       }
     }
-    if (y >= 1000) {
-      y = 999;
+    if (y >= h ){
+      y = rand() % 1000;
     } else {
-      if (y < 0 ) {
-        y = 0;
+      if (y < 0 ){
+        y = rand() % 1000;
       } else {
         y = y + rand() % 3 * (int)(pow(-1, i));
       }
